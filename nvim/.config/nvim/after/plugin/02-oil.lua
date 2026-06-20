5fe03753f2978ec499aefeb6531061c4733f9361
Nam Nguyen <nguyenvietnam2401@gmail.com>
2026-06-21 01:25:39 +1000

    nvim: set oil columns

--- nvim/.config/nvim/after/plugin/02-oil.lua
+++ nvim/.config/nvim/after/plugin/02-oil.lua
@@ -2,6 +2,12 @@ local oil = require("oil")
 
 oil.setup({
     delete_to_trash = true,
+    columns = {
+        "permissions",
+        "size",
+        "mtime",
+        "birthtime",
+    },
     view_options = {
         show_hidden = true,
     },
